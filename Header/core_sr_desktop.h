fc1aba4b5518530e248e256a0d87128f3db26172
dreais <valentin.lattay@gmail.com>
2019-10-15 23:28:14 +0200

    initial repo

---
+++ Header/core_sr_desktop.h
@@ -0,0 +1,14 @@
+//
+// Created by Valentin on 10/15/2019.
+//
+
+#ifndef SR_DESKTOP_CORE_SR_DESKTOP_H
+#define SR_DESKTOP_CORE_SR_DESKTOP_H
+
+#pragma once
+
+#ifdef _WIN32
+#include "soundio.h"
+#endif
+
+#endif //SR_DESKTOP_CORE_SR_DESKTOP_H
---
+++ Sources/main.c
@@ -0,0 +1,10 @@
+//
+// Created by Valentin on 10/15/2019.
+//
+
+#include "../Header/core_sr_desktop.h"
+
+int main(void)
+{
+	return 0;
+}
