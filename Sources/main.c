9dbaac8bf168d7e2f4891197c8f26df8790bf9c1
dreais <valentin.lattay@gmail.com>
2019-10-16 10:54:40 +0200

    selecting the default sound output

--- Sources/main.c
+++ Sources/main.c
@@ -2,9 +2,21 @@
 // Created by Valentin on 10/15/2019.
 //
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "../Header/core_sr_desktop.h"
 
 int main(void)
 {
+	struct SoundIo *soundio = soundio_create();
+	struct SoundIoDevice *selected = NULL;
+
+	int default_device;
+
+	soundio_connect(soundio);
+	soundio_flush_events(soundio);
+	default_device = soundio_default_output_device_index(soundio);
+	selected = soundio_get_output_device(soundio, default_device);
+	soundio_device_sort_channel_layouts(selected);
 	return 0;
 }
